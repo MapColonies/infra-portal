9fa4edfb58dd49030edbebebb3f6bdba67bde313
CptSchnitz <CptSchnitz@users.noreply.github.com>
2024-10-14 12:17:45 +0000

    Deploy preview for PR 20 🛫

---
+++ pr-preview/pr-20/assets/js/5e95c892.be660302.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkinfra_portal=self.webpackChunkinfra_portal||[]).push([[9647],{7121:(e,r,a)=>{a.r(r),a.d(r,{default:()=>p});a(6540);var s=a(4164),n=a(1003),t=a(7559),u=a(2831),c=a(4837),l=a(4848);function p(e){return(0,l.jsx)(n.e3,{className:(0,s.A)(t.G.wrapper.docsPages),children:(0,l.jsx)(c.A,{children:(0,u.v)(e.route.routes)})})}}}]);
