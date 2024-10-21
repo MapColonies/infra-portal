2bbbe9012558b1dfadaefc7d8210d7a3b5f6c052
alex131290 <alex131290@users.noreply.github.com>
2024-10-21 13:57:35 +0000

    Remove preview for PR 19 🛬

--- pr-preview/pr-19/assets/js/a7456010.01b17a0d.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkinfra_portal=self.webpackChunkinfra_portal||[]).push([[1235],{8552:a=>{a.exports=JSON.parse('{"name":"docusaurus-plugin-content-pages","id":"default"}')}}]);
