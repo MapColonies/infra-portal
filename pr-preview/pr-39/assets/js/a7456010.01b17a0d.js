3deb80d5d96194b3777780828bae11778b0a1247
netanelC <netanelC@users.noreply.github.com>
2025-07-20 13:03:17 +0000

    Deploy preview for PR 39 🛫

---
+++ pr-preview/pr-39/assets/js/a7456010.01b17a0d.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkinfra_portal=self.webpackChunkinfra_portal||[]).push([[1235],{8552:a=>{a.exports=JSON.parse('{"name":"docusaurus-plugin-content-pages","id":"default"}')}}]);
